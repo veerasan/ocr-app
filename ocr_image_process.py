bb70e4292148e56bd0cedc857ccf4313f5d170cc
veeraguru sangarapandiyan <veeraguru@veeragurus-MacBook-Pro.local>
2020-12-22 08:11:57 +0000

    change outfile from .txt to .json

--- ocr_image_process.py
+++ ocr_image_process.py
@@ -4,8 +4,9 @@ import cv2
 from pytesseract import Output
 # import re
 from config import get_config
-import os, uuid
+import os, uuid, time, json
 from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient, __version__
+
 
 # app = Flask(__name__)
 
@@ -30,12 +31,17 @@ def download_blob_data():
 
 
 def upload_blob_data(p_result):
+    result_dict = {}
     container_name = get_config('RESULT_CONTAINER')
     transaction_id = blob_file_name.split(".")[0]
-    local_file_name = transaction_id + str(uuid.uuid4()) + ".txt"
-    with open(local_file_name, 'w') as fr:
-        fr.write(p_result[0])
-        fr.close()
+    local_file_name = transaction_id + str(uuid.uuid4()) + ".json"
+    txn_date_time = time.strftime("%d-%m-%Y-%H%M%S"+" GMT")
+    result_dict['transaction_id'] = transaction_id
+    result_dict['supplier_code'] = p_result[0]
+    result_dict['transaction_date_time'] = txn_date_time
+    with open(local_file_name, 'w') as outfile:
+        json.dumps(result_dict, outfile, indent=4)
+        outfile.close()
     blob_client = blob_service_client.get_blob_client(container=container_name, blob=local_file_name)
     with open(local_file_name, "rb") as data:
         blob_client.upload_blob(data)
